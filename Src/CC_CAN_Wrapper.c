bb0a44170749b31adbe17e2a6cc9af117224a3d1
Alex Lossberg <50721703+LDwhoRU@users.noreply.github.com>
2020-11-12 02:30:57 +1000

    Removed device specific dependencies

--- Src/CC_CAN_Wrapper.c
+++ Src/CC_CAN_Wrapper.c
@@ -9,11 +9,14 @@
 
 #include "CC_CAN_Wrapper.h"
 
-void Send_CC_FatalShutdown(char* errorCause, bool echo)
+void Send_CC_FatalShutdown(char* errorCause, bool echo,
+		uint32_t* CAN1_Mailbox, uint32_t* CAN2_Mailbox, uint32_t* CAN3_Mailbox,
+		CAN_HandleTypeDef* CanHandle, CAN_HandleTypeDef* CanHandle2, CAN_HandleTypeDef* CanHandle3,
+		UART_HandleTypeDef* huartHandle)
 {
 	if(echo)
 	{
-		HAL_UART_Transmit(&huart3, (uint8_t *)errorCause, (size_t)strlen(errorCause), HAL_MAX_DELAY);
+		HAL_UART_Transmit(huartHandle, (uint8_t *)errorCause, (size_t)strlen(errorCause), HAL_MAX_DELAY);
 	}
 	CC_FatalShutdown_t fatalShutdown = Compose_CC_FatalShutdown();
 	CAN_TxHeaderTypeDef header =
@@ -25,9 +28,15 @@ void Send_CC_FatalShutdown(char* errorCause, bool echo)
 			.TransmitGlobalTime = DISABLE,
 	};
 	uint8_t data[1] = {0xF};
-	HAL_CAN_AddTxMessage(&hcan1, &header, data, &CC_GlobalState->CAN1_TxMailbox);
-	HAL_CAN_AddTxMessage(&hcan2, &header, data, &CC_GlobalState->CAN2_TxMailbox);
-	HAL_CAN_AddTxMessage(&hcan3, &header, data, &CC_GlobalState->CAN3_TxMailbox);
+	HAL_CAN_AddTxMessage(CanHandle, &header, data, CAN1_Mailbox);
+	HAL_CAN_AddTxMessage(CanHandle2, &header, data, CAN2_Mailbox);
+	HAL_CAN_AddTxMessage(CanHandle3, &header, data, CAN3_Mailbox);
+
+	if(osSemaphoreAcquire(CC_GlobalState->sem, SEM_ACQUIRE_TIMEOUT) == osOK)
+	{
+		CC_GlobalState->ccInit = false;
+		osSemaphoreRelease(CC_GlobalState->sem);
+	}
 }
 
 #endif
