64c4dd2e19703c88ba7db58375dc1c5c173f4515
Calvin <calvinofthejs@gmail.com>
2020-11-11 23:01:46 +1000

    update PDM messages for set channel state

--- Inc/PDM_CAN_Messages.h
+++ Inc/PDM_CAN_Messages.h
@@ -18,7 +18,7 @@
 /**
  * @brief PDM Initiate Startup Message
  */
-typedef struct PDM_InitiateStartup_t
+typedef struct PDM_InitiateStartup
 {
 	uint32_t id; /**< CAN Packet ID */
 } PDM_InitiateStartup_t;
@@ -33,7 +33,7 @@ PDM_InitiateStartup_t Compose_PDM_InitiateStartup(void);
 /**
  * @brief PDM Startup Ok Message
  */
-typedef struct PDM_StartupOk_t
+typedef struct PDM_StartupOk
 {
 	uint32_t id; /**< CAN Packet ID*/
 	uint8_t data[4]; /**< Data */
@@ -51,12 +51,12 @@ PDM_StartupOk_t Compose_PDM_StartupOk(uint32_t powerChannels);
  * @param packet The PDM_StartupOk_t packet to parse
  * @param 32 bit string containing Boolean-like values of active power status at startup
  */
-void Parse_PDM_StartupOk(PDM_StartupOk_t packet, uint32_t* powerChannels);
+void Parse_PDM_StartupOk(uint8_t data[4], uint32_t *powerChannels);
 
 /**
  * @brief PDM Select Channels for Startup Message
  */
-typedef struct PDM_SelectStartup_t
+typedef struct PDM_SelectStartup
 {
 	uint32_t id; /**< CAN Packet ID*/
 	uint8_t data[4]; /**< Data */
@@ -79,7 +79,7 @@ void Parse_PDM_SelectStartup(PDM_SelectStartup_t packet, uint32_t* powerChannels
 /**
  * @brief PDM Set Channel States Message
  */
-typedef struct PDM_SetChannelStates_t
+typedef struct PDM_SetChannelStates
 {
 	uint32_t id; /**< CAN Packet ID*/
 	uint8_t data[4]; /**< Data */
@@ -97,9 +97,9 @@ PDM_SetChannelStates_t Compose_PDM_SetChannelStates(uint32_t powerChannels);
  * @param packet The PDM_SelectStartup_t packet to parse
  * @param 32 bit string containing Boolean-like values power channels toggled to
  */
-void Parse_PDM_SetChannelStates(PDM_SetChannelStates_t packet, uint32_t* powerChannels);
+void Parse_PDM_SetChannelStates(uint8_t data[4], uint32_t* powerChannels);
 
-typedef struct PDM_Heartbeat_t
+typedef struct PDM_Heartbeat
 {
 	uint32_t id; /**< CAN Packet ID*/
 	uint8_t data[4]; /**< Data */
@@ -110,13 +110,13 @@ typedef struct PDM_Heartbeat_t
  * @param 32 bit string containing Boolean-like values to set power status to
  * @return The generated PDM_Heartbeat_t packet
  */
-PDM_Heartbeat_t Compose_PDM_Heartbeat(uint8_t powerChannels[4]);
+PDM_Heartbeat_t Compose_PDM_Heartbeat(uint32_t powerChannels);
 
 /**
  * @brief PDM Heartbeat Message Parser
  * @param packet The PDM_Heartbeat_t packet to parse
  * @param 32 bit string containing Boolean-like values power channels toggled to
  */
-void Parse_PDM_Heartbeat(PDM_Heartbeat_t packet, uint8_t powerChannels[4]);
+void Parse_PDM_Heartbeat(uint8_t data[4], uint32_t *powerChannels);
 
 #endif /* INC_PDM_CAN_MESSAGES_H_ */
--- Src/PDM_CAN_Messages.c
+++ Src/PDM_CAN_Messages.c
@@ -11,27 +11,28 @@
 
 PDM_InitiateStartup_t Compose_PDM_InitiateStartup(void) {
 	PDM_InitiateStartup_t p;
-	p.id = Compose_CANId(0x2, 0x14, 0x0, 0x2, 0x00, 0x0);
+	p.id = Compose_CANId(CAN_PRIORITY_NORMAL, CAN_SRC_ID_PDM, 0x0,
+	CAN_TYPE_RECEIVE, 0x00, 0x0);
 	return p;
 }
 
 PDM_StartupOk_t Compose_PDM_StartupOk(uint32_t powerChannels) {
 	PDM_StartupOk_t packet;
-	packet.id = Compose_CANId(0x2, 0x14, 0x0, 0x3, 0x00, 0x0);
+	packet.id = Compose_CANId(CAN_PRIORITY_NORMAL, CAN_SRC_ID_PDM, 0x0,
+	CAN_TYPE_TRANSMIT, 0x00, 0x0);
 
-	uint8_t d[4] = { 0 };
-	for (int i = 0; i < 4; ++i)
-		d[i] = ((uint8_t*) &powerChannels)[3 - i];
-
-	for (int i = 0; i < 4; ++i)
-		packet.data[i] = (uint8_t) (d[i] & 0xFF);
+	for (int i = 0; i < 4; i++) {
+		packet.data[i] = (powerChannels >> (i * 8)) & 0xFF;
+	}
 
 	return packet;
 }
 
-void Parse_PDM_StartupOk(PDM_StartupOk_t packet, uint32_t *powerChannels) {
-	*powerChannels = (packet.data[0] << 24) + (packet.data[1] << 16)
-			+ (packet.data[2] << 8) + packet.data[3];
+void Parse_PDM_StartupOk(uint8_t data[4], uint32_t *powerChannels) {
+	*powerChannels = 0;
+	for (int i = 0; i < 4; i++) {
+		*powerChannels |= data[i] << (i * 8);
+	}
 }
 
 PDM_SelectStartup_t Compose_PDM_SelectStartup(uint32_t powerChannels) {
@@ -56,40 +57,40 @@ void Parse_PDM_SelectStartup(PDM_SelectStartup_t packet,
 
 PDM_SetChannelStates_t Compose_PDM_SetChannelStates(uint32_t powerChannels) {
 	PDM_SetChannelStates_t packet;
-	packet.id = Compose_CANId(0x2, 0x14, 0x0, 0x2, 0x02, 0x0);
-
-	uint8_t d[4] = { 0 };
-	for (int i = 0; i < 4; ++i)
-		d[i] = ((uint8_t*) &powerChannels)[3 - i];
-
-	for (int i = 0; i < 4; ++i)
-		packet.data[i] = (uint8_t) (d[i] & 0xFF);
-
-	return packet;
-}
-
-void Parse_PDM_SetChannelStates(PDM_SetChannelStates_t packet,
-		uint32_t *powerChannels) {
-	*powerChannels = (packet.data[0] << 24) + (packet.data[1] << 16)
-			+ (packet.data[2] << 8) + packet.data[3];
-}
-
-PDM_Heartbeat_t Compose_PDM_Heartbeat(uint8_t powerChannels[8]) {
-	PDM_Heartbeat_t packet;
-	packet.id = Compose_CANId(CAN_PRIORITY_HEARTBEAT, CAN_SRC_ID_PDM, 0x0,
-			CAN_TYPE_HEARTBEAT, 0x0, 0x0);
+	packet.id = Compose_CANId(CAN_PRIORITY_NORMAL, CAN_SRC_ID_PDM, 0x0,
+	CAN_TYPE_RECEIVE, 0x02, 0x0);
 
 	for (int i = 0; i < 4; i++) {
-		packet.data[i] = powerChannels[2*i] | (powerChannels[2*i+1] << 4);
+		packet.data[i] = (powerChannels >> (i * 8)) & 0xFF;
 	}
 
 	return packet;
 }
 
-void Parse_PDM_Heartbeat(PDM_Heartbeat_t packet, uint8_t powerChannels[8]) {
+void Parse_PDM_SetChannelStates(uint8_t data[4],
+		uint32_t *powerChannels) {
+	*powerChannels = 0;
 	for (int i = 0; i < 4; i++) {
-		powerChannels[2*i] = packet.data[i] & 0xF;
-		powerChannels[2*i + 1] = (packet.data[i] >> 4) & 0xF;
+		*powerChannels |= data[i] << (i * 8);
+	}
+}
+
+PDM_Heartbeat_t Compose_PDM_Heartbeat(uint32_t powerChannels) {
+	PDM_Heartbeat_t packet;
+	packet.id = Compose_CANId(CAN_PRIORITY_HEARTBEAT, CAN_SRC_ID_PDM, 0x0,
+	CAN_TYPE_HEARTBEAT, 0x0, 0x0);
+
+	for (int i = 0; i < 4; i++) {
+		packet.data[i] = (powerChannels >> (i * 8)) & 0xFF;
+	}
+
+	return packet;
+}
+
+void Parse_PDM_Heartbeat(uint8_t data[4], uint32_t *powerChannels) {
+	*powerChannels = 0;
+	for (int i = 0; i < 4; i++) {
+		*powerChannels |= data[i] << (i * 8);
 	}
 }
 
