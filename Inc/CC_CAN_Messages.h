97ef76a367ba739c03184841b56a33d2f762953d
AlexLossberg <alexlossberg@gmail.com>
2020-11-13 08:01:52 +1000

    Working inverter request

--- Inc/CC_CAN_Messages.h
+++ Inc/CC_CAN_Messages.h
@@ -59,4 +59,28 @@ typedef struct CC_SoftShutdown_t
  */
 CC_SoftShutdown_t Compose_CC_SoftShutdown(void);
 
+
+/**
+ * @brief Chassis Controller Inverter Motor RPM Request Message
+ */
+typedef struct CC_RequestRPM_t
+{
+	uint32_t id; /**< CAN Packet ID*/
+	uint8_t data[8]; /**< Data */
+} CC_RequestRPM_t;
+
+/**
+ * @brief Chassis Controller Inverter Motor RPM Request Message Composer
+ * @return The generated BMS_BadCellVoltage_t packet
+ */
+CC_RequestRPM_t Compose_CC_RequestRPM(uint16_t nodeId);
+
+/**
+ * @brief Chassis Controller Inverter Motor RPM Request Message Parser
+ * @param data The CC_RequestRPM_t packet data to parse
+ * @param motorRPM The BMS ID
+ */
+void Parse_CC_RequestRPM(uint8_t* data, int16_t* motorRPM);
+
+
 #endif /* INC_CC_CAN_MESSAGES_H_ */
--- Src/CC_CAN_Messages.c
+++ Src/CC_CAN_Messages.c
@@ -30,4 +30,25 @@ CC_SoftShutdown_t Compose_CC_SoftShutdown(void)
 	return p;
 }
 
+CC_RequestRPM_t Compose_CC_RequestRPM(uint16_t nodeId)
+{
+	CC_RequestRPM_t p;
+	uint16_t index = 0x210A;
+	p.id = 0x600 + nodeId; // 0x600 for Query + Node ID Specifier
+	p.data[0] = 0b01001100; // Client Command Specifier + Number of Bytes + xx
+	p.data[1] = (uint8_t)index & 0xFF; // Index
+	p.data[2] = (uint8_t)(index >> 8) & 0xFF; // Index
+	p.data[3] = 0x01; // Subindex
+	p.data[4] = 0x00; // Data
+	p.data[5] = 0x00; // Data
+	p.data[6] = 0x00; // Data
+	p.data[7] = 0x00; // Data
+	return p;
+}
+
+void Parse_CC_RequestRPM(uint8_t* data, int16_t* motorRPM)
+{
+	*motorRPM = data[5] << 8 | data[4];
+}
+
 #endif
