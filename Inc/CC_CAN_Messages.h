86244b62e202827e1e2ee7f2e76ce31f8c464f43
AlexLossberg <alexlossberg@gmail.com>
2020-11-13 21:31:16 +1000

    Added torque commands and shutdown

--- Inc/CC_CAN_Messages.h
+++ Inc/CC_CAN_Messages.h
@@ -71,16 +71,59 @@ typedef struct CC_RequestRPM_t
 
 /**
  * @brief Chassis Controller Inverter Motor RPM Request Message Composer
- * @return The generated BMS_BadCellVoltage_t packet
+ * @return The generated CC_RequestRPM_t packet
  */
 CC_RequestRPM_t Compose_CC_RequestRPM(uint16_t nodeId);
 
 /**
  * @brief Chassis Controller Inverter Motor RPM Request Message Parser
  * @param data The CC_RequestRPM_t packet data to parse
- * @param motorRPM The BMS ID
+ * @param motorRPM Address of where MotorRPM can be stored
  */
 void Parse_CC_RequestRPM(uint8_t* data, int16_t* motorRPM);
+
+/**
+ * @brief Chassis Controller Inverter Motor Command Message
+ */
+typedef struct CC_MotorCommand_t
+{
+	uint32_t id; /**< CAN Packet ID*/
+	uint8_t data[8]; /**< Data */
+} CC_MotorCommand_t;
+
+/**
+ * @brief Chassis Controller Inverter Motor Command Message Composer
+ * @return The generated CC_MotorCommand_t packet
+ */
+CC_MotorCommand_t Compose_CC_MotorCommand(uint16_t nodeId, int32_t motorCommand, uint8_t motorId);
+
+/**
+ * @brief Chassis Controller Inverter Motor Command Message Parser
+ * @param data The CC_MotorCommand_t packet data to parse
+ * @param motorCommand The desired duty cycle
+ */
+void Parse_CC_MotorCommand(uint8_t* data, int32_t* motorCommand);
+
+/**
+ * @brief Chassis Controller Inverter Emergency Shutdown Command Message
+ */
+typedef struct CC_ShutdownInverter_t
+{
+	uint32_t id; /**< CAN Packet ID*/
+	uint8_t data[8]; /**< Data */
+} CC_ShutdownInverter_t;
+
+/**
+ * @brief Chassis Controller Inverter Emergency Shutdown Command Message Composer
+ * @return The generated CC_ShutdownInverter_t packet
+ */
+CC_ShutdownInverter_t Compose_CC_ShutdownInverter(uint16_t nodeId);
+
+/**
+ * @brief Chassis Controller Inverter Emergency Shutdown Command Message Parser
+ * @param data The CC_ShutdownInverter_t packet data to parse
+ */
+void Parse_CC_ShutdownInverter(uint8_t* data);
 
 
 #endif /* INC_CC_CAN_MESSAGES_H_ */
--- Src/CC_CAN_Messages.c
+++ Src/CC_CAN_Messages.c
@@ -51,4 +51,45 @@ void Parse_CC_RequestRPM(uint8_t* data, int16_t* motorRPM)
 	*motorRPM = data[5] << 8 | data[4];
 }
 
+CC_MotorCommand_t Compose_CC_MotorCommand(uint16_t nodeId, int32_t motorCommand, uint8_t motorId)
+{
+	CC_MotorCommand_t p;
+	uint16_t index = 0x2000;
+	p.id = 0x600 + nodeId; // 0x600 for Query + Node ID Specifier
+	p.data[0] = 0b00110000; // Client Command Specifier + Number of Bytes + xx
+	p.data[1] = (uint8_t)index & 0xFF; // Index
+	p.data[2] = (uint8_t)(index >> 8) & 0xFF; // Index
+	p.data[3] = motorId; // Subindex
+	p.data[4] = (uint8_t)motorCommand & 0xFF; // Data
+	p.data[5] = (uint8_t)(motorCommand >> 8) & 0xFF; // Data
+	p.data[6] = (uint8_t)(motorCommand >> 16) & 0xFF; // Data
+	p.data[7] = (uint8_t)(motorCommand >> 24) & 0xFF; // Data
+	return p;
+}
+
+void Parse_CC_RequestCommand(uint8_t* data, int32_t* motorCommand)
+{
+	return;
+}
+
+CC_ShutdownInverter_t Compose_CC_ShutdownInverter(uint16_t nodeId)
+{
+	CC_ShutdownInverter_t p;
+	uint16_t index = 0x200C;
+	p.id = 0x600 + nodeId; // 0x600 for Query + Node ID Specifier
+	p.data[0] = 0b00111100; // Client Command Specifier + Number of Bytes + xx
+	p.data[1] = (uint8_t)index & 0xFF; // Index
+	p.data[2] = (uint8_t)(index >> 8) & 0xFF; // Index
+	p.data[3] = 0x00; // Subindex
+	p.data[4] = 0x00; // Data
+	p.data[5] = 0x00; // Data
+	p.data[6] = 0x00; // Data
+	p.data[7] = 0x00; // Data
+
+}
+
+void Parse_CC_ShutdownInverter(uint8_t* data){
+	return;
+}
+
 #endif
