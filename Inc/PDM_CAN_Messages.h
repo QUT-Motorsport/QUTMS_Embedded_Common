d88cd474d088ebf3c0b7f93152d444d8e28685e2
Tom <tr.fraser@bigpond.com>
2020-11-18 19:05:06 +1000

    Fixed Startup ID, power channels calc

--- Inc/PDM_CAN_Messages.h
+++ Inc/PDM_CAN_Messages.h
@@ -19,7 +19,7 @@
 #define LV_STARTUP 0xB08D1F50
 #define HV_STARTUP 0x06628029
 #define BRAKE_LIGHT_MASK 0x00000080
-#define PDM_POWER_CC_MASK 0x91000000
+#define PDM_POWER_CC_MASK 0x9100000
 
 /**
  * @brief PDM Initiate Startup Message
