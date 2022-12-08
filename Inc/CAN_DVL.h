7b2ca2e2065cd22ea123937b1f25322e51a272c3
Calvin <calvinofthejs@gmail.com>
2022-12-09 10:03:38 +1100

    fix DVL stuff so compile

--- Inc/CAN_DVL.h
+++ Inc/CAN_DVL.h
@@ -57,14 +57,14 @@ void Parse_DVL_Heartbeat(uint8_t *data, DVL_HeartbeatState_t *state);
 typedef union DVL_DrivingDynamics1_Data {
 	uint8_t data[8];
 	struct {
-	uint8_t speed_actual : 8;
-	uint8_t speed_target : 8;
-	int8_t steering_angle_actual : 8;
-	int8_t steering_angle_target : 8;
-	uint8_t brake_hydr_actual : 8;
-	uint8_t brake_hydr_target : 8;
-	int8_t motor_moment_actual : 8;
-	int8_t motor_moment_target : 8;
+		uint8_t speed_actual : 8;
+		uint8_t speed_target : 8;
+		int8_t steering_angle_actual : 8;
+		int8_t steering_angle_target : 8;
+		uint8_t brake_hydr_actual : 8;
+		uint8_t brake_hydr_target : 8;
+		int8_t motor_moment_actual : 8;
+		int8_t motor_moment_target : 8;
 	} _fields;
 } DVL_DrivingDynamics1_Data_u;
 
@@ -75,17 +75,44 @@ typedef struct DVL_DrivingDynamics1 {
 
 DVL_DrivingDynamics1_t Compose_DVL_DrivingDynamics1(DVL_DrivingDynamics1_Data_u *dynamics);
 
+enum DVL_AS_State {
+	DVL_AS_STATE_OFF = 0x1,
+	DVL_AS_STATE_READY = 0x2,
+	DVL_AS_STATE_DRIVING = 0x3,	
+	DVL_AS_STATE_EBRAKE = 0x4,
+	DVL_AS_STATE_FINISH = 0x5
+};
+
+enum DVL_EBS_State {
+	DVL_EBS_STATE_UNAVAILABLE = 0x1,
+	DVL_EBS_STATE_ARMED = 0x1,
+	DVL_EBS_STATE_ACTIVATED = 0x2
+};
+
+enum DVL_AMI_State {
+	DVL_AMI_STATE_ACCEL = 0x1,
+	DVL_AMI_STATE_SKIDPAD = 0x2,
+	DVL_AMI_STATE_TRACKDRIVE = 0x3,
+	DVL_AMI_STATE_BRAKETEST = 0x4,
+	DVL_AMI_STATE_INSPECTION = 0x5,
+	DVL_AMI_STATE_AUTOCROSS = 0x6
+};
+
+enum DVL_SERVICE_BRAKE_State {
+	DVL_SERVICE_BRAKE_STATE_AVAILABLE = 0x3
+};
+
 typedef union DVL_SystemStatus_Data {
 	uint8_t data[5];
 	struct {
-	uint8_t AS_state : 3;
-	uint8_t EBS_state : 2;
-	uint8_t AMI_state : 3;
-	uint8_t steering_state : 1;
-	uint8_t service_brake_state : 2;
-	uint8_t lap_counter : 4;
-	uint8_t cones_count_actual : 8;
-	uint32_t cones_count_all : 17;
+		uint8_t AS_state : 3;
+		uint8_t EBS_state : 2;
+		uint8_t AMI_state : 3;
+		uint8_t steering_state : 1;
+		uint8_t service_brake_state : 2;
+		uint8_t lap_counter : 4;
+		uint8_t cones_count_actual : 8;
+		uint32_t cones_count_all : 17;
 	} _fields;
 } DVL_SystemStatus_Data_u;
 
