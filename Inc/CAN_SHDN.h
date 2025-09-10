0a9225c84154ae19aec89e2f47da4e0d552d0377
Sean C <s.d.camp179@gmail.com>
2025-09-10 20:36:47 +1000

    Shutdown board heartbeat flags, compose and parse

--- Inc/CAN_SHDN.h
+++ Inc/CAN_SHDN.h
@@ -11,6 +11,7 @@
 #include <QUTMS_can.h>
 #include "stdbool.h"
 #include <stdlib.h>
+#include <stdint.h>
 
 /**
  * @brief Shutdown Heartbeat Request Message
@@ -19,6 +20,35 @@ typedef struct SHDN_ShutdownTriggered_t
 {
 	uint32_t id; /**< CAN Packet ID */
 } SHDN_ShutdownTriggered_t;
+
+typedef struct SHDN_flags {
+	uint16_t rawMem;
+	struct {
+		uint8_t F_Harness : 1;
+		uint8_t Junct_Box : 1;
+		uint8_t L_Upright : 1;
+		uint8_t R_Upright : 1;
+		uint8_t Accu : 1;
+		uint8_t Roll_Hoop : 1;
+		uint8_t HVD : 1;
+		uint8_t DVL : 1;
+		uint8_t SPARE : 1;
+		uint8_t TS_KEY : 1;
+	} _SHDN_Flags;
+} SHDN_Flags_u;
+
+typedef struct SHDN_HeartbeatState {
+	uint8_t firstFault;
+	SHDN_Flags_u flags;
+} SHDN_HeartbeatState_t;
+
+typedef struct SHDN_Heartbeat {
+	uint32_t id;
+	uint8_t data[8];
+} SHDN_Heartbeat_t;
+
+SHDN_Heartbeat_t Compose_SHDN_Heartbeat(SHDN_HeartbeatState_t* state);
+void Parse_SHDN_Heartbeat(uint8_t data[3], SHDN_HeartbeatState_t* state);
 
 /**
  * @brief Shutdown Heartbeat Request Message Composer
--- Src/CAN_SHDN.c
+++ Src/CAN_SHDN.c
@@ -9,6 +9,30 @@
 
 #include "CAN_SHDN.h"
 
+SHDN_Heartbeat_t Compose_SHDN_Heartbeat(SHDN_HeartbeatState_t *state) {
+	SHDN_Heartbeat_t msg;
+
+	msg.id = SHDN_Heartbeat_ID;
+
+	msg.data[0] = state->firstFault;
+	msg.data[1] = (state->flags.rawMem) & 0xFF;
+	msg.data[2] = (state->flags.rawMem >> 8) & 0xFF;
+
+	// clear unused bytes
+	msg.data[3] = 0;
+	msg.data[4] = 0;
+	msg.data[5] = 0;
+	msg.data[6] = 0;
+	msg.data[7] = 0;
+
+	return msg;
+}
+
+void Parse_SW_Heartbeat(uint8_t data[3], SHDN_HeartbeatState_t *state) {
+	state->firstFault = data[0];
+	state->flags.rawMem = (data[1] | (data[2] << 8));
+}
+
 SHDN_ShutdownTriggered_t Compose_SHDN_ShutdownTriggered()
 {
 	SHDN_ShutdownTriggered_t p;
