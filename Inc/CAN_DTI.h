0a3bad2b8469ee3ebd4200c4e072f854e54e88c9
Chloe B <crbarron18@gmail.com>
2026-09-14 16:09:18 +1000

    feat: add DTI control messages

--- Inc/CAN_DTI.h
+++ Inc/CAN_DTI.h
@@ -1,7 +1,8 @@
 /*
  * CAN_DTI.h
  *
- *  Created on: July 4, 2026
+ *  Created in: 2026
+ *  Author: Chloe B
  *
  */
 
@@ -19,9 +20,28 @@ typedef enum {
 
 /* Packet IDs */
 typedef enum {
-    DTI_PID_ERPM_DUTY_VOLT = 0x20,
-    DTI_PID_Currents = 0x21,
-    DTI_PID_SET_EPRM = 0x03,
+    // messages sent from inverter
+    DTI_PID_S1 = 0x1F, // CONTROL STATUS - Control mode, Target Iq, motor position, isMotorStill
+    DTI_PID_S2 = 0x20, // ERPM_DUTY_VOLT - ERPM, Duty, Input Voltage
+    DTI_PID_S3 = 0x21, // CURRENTS_READ - AC Current, DC Current
+    DTI_PID_S4 = 0x22, // TEMPS_FAULTS - Controller Temp., Motor Temp., Fault code
+    DTI_PID_S5 = 0x23, // FOC - Id, Iq values
+    DTI_PID_S6 = 0x24, // CONTROL_DATA - Throttle signal, Brake signal, Digital I/Os, Drive enable, Limit status bits, CAN map version
+    DTI_PID_S7 = 0x25, // AC_SET - configured max AC current, available max AC current, configured min AC current, available min AC current
+    DTI_PID_S8 = 0x26, // DC_SET - configured max DC current, available max DC current, configured min DC current, available min DC current
+    // Commands to Inverter
+    DTI_PID_C1 = 0x01, //SET_CURRENT - 
+    DTI_PID_C2 = 0x02, // SET_BRAKE_CURRENT - 
+    DTI_PID_C3 = 0x03, // SET_EPRM - 
+    DTI_PID_C4 = 0x04, // SET_POS - 
+    DTI_PID_C5 = 0x05, // SET_REL_CURRENT - 
+    DTI_PID_C6 = 0x06, // SET_REL_BRAKECURRENT - 
+    DTI_PID_C7 = 0x07, // SET_DIG_OUTPUT - 
+    DTI_PID_C8 = 0x08, // SET_MAX_AC_CURRENT - 
+    DTI_PID_C9 = 0x09, // SET_MAX_AC_BRAKECURRENT - 
+    DTI_PID_C10 = 0x0A, // SET_MAX_DC_CURRENT - 
+    DTI_PID_C11 = 0x0B, // SET_MAX_DC_BRAKECURRENT - 
+    DTI_PID_C12 = 0x0C, // DRIVE_ENABLE - 
 } dti_pid_t;
 
 /* DTI Fault Codes */
@@ -40,6 +60,19 @@ typedef enum
     DTI_FAULT_ANALOG_INPUT = 0x0A,
 } DTI_FAULTS_t;
 
+typedef enum {
+    DTI_DRIVE_ALLOWED = 1,
+    DTI_DRIVE_NOT_ALLOWED = 0,
+} DTI_drive_status_t;
+
+/* ID */
+// Extended CAN ID = Packet ID (bits 28:8) | Node ID (bits 7:0) -> shift the packet ID by 8
+uint32_t dti_msg_id(dti_pid_t pid, unit32_t node);
+
+// OLD
+// #define dti_msg_id(pid, node)  ((((uint32_t)(pid)) << 8) | ((uint32_t)(node) & 0xFF))
+// #define dti_get_packet(id)  ((dti_pid_t)(((id) >> 8)))
+// #define dti_get_node(id) ((dti_node_t)((id) & 0xFF))
 
 /* Plain Message */
 typedef struct {
@@ -47,16 +80,24 @@ typedef struct {
     uint8_t data[8];
 } dti_msg_t;
 
-/* ID */
 
-// Extended CAN ID = Packet ID (bits 28:8) | Node ID (bits 7:0) -> shift the packet ID by 8
-#define dti_msg_id(pid, node)  ((((uint32_t)(pid)) << 8) | ((uint32_t)(node) & 0xFF))
-#define dti_get_packet(id)  ((dti_pid_t)(((id) >> 8)))
-#define dti_get_node(id) ((dti_node_t)((id) & 0xFF))
+
+// use a predefined packetID, require the node (destination) to be parsed when sending a messsing
 
 
 /* COMMANDS TO INVERTER */
-dti_msg_t Compose_setERPM(dti_node_t node, int32_t erpm);
+dti_msg_t Compose_C1(dti_node_t node, int32_t ac_curr);
+dti_msg_t Compose_C2(dti_node_t node, int32_t brake_curr);
+dti_msg_t Compose_C3(dti_node_t node, int32_t erpm);
+dti_msg_t Compose_C4(dti_node_t node, int32_t target_pos)
+dti_msg_t Compose_C5(dti_node_t node, int32_t rel_ac_curr)
+dti_msg_t Compose_C6(dti_node_t node, int32_t rel_brake_curr)
+// Copose 7
+dti_msg_t Compose_C8(dti_node_t node, int32_t max_ac_curr)
+dti_msg_t Compose_C9(dti_node_t node, int32_t max_brake_curr)
+dti_msg_t Compose_C10(dti_node_t node, int32_t max_dc_curr)
+dti_msg_t Compose_C11(dti_node_t node, int32_t max_dc_brake_curr)
+dti_msg_t Compose_C12(dti_node_t node, int16_t driveEnableStat)
 
 
 /* TRANSMITTED BY INVERTER */
--- Src/CAN_DTI.c
+++ Src/CAN_DTI.c
@@ -1,26 +1,237 @@
 /*
  * CAN_DTI.c
  *
- *  Created on: July 4, 2026
- *      Author: Chloe
+ *  Created in: 2026
+ *  Author: Chloe B
  */
 
 #include <CAN_DTI.h>
 
 #ifdef QUTMS_CAN_DTI
 
-// Construct an ID 
-dti_msg_t Compose_setERPM(dti_node_t node, int32_t erpm)
+// DTI 
+uint32_t dti_msg_id(dti_pid_t pid, uint32_t node) {
+	uint32_t id = ((((uint32_t)(pid)) << 8) | ((uint32_t)(node) & 0xFF));
+	return id;
+}
+
+/* COMPOSE CONTROL MESSAGES*/
+
+// Set AC Current
+dti_msg_t Compose_C1(dti_node_t node, int32_t ac_curr)
 {
     dti_msg_t msg;
 
-    msg.id = dti_msg_id(DTI_PID_SET_EPRM, node);
+    msg.id = dti_msg_id(DTI_PID_C1, node);
+    // 0-1 AC Current
+    msg.data[0] = (ac_curr >> 8);
+    msg.data[1] = ac_curr;
+    // 2-7 not used
+    msg.data[2] = 0;
+    msg.data[3] = 0;
+    msg.data[4] = 0;
+    msg.data[5] = 0;
+    msg.data[6] = 0;
+    msg.data[7] = 0;
 
+    return msg;
+}
+
+// Set Brake Current
+dti_msg_t Compose_C2(dti_node_t node, int32_t brake_curr)
+{
+    dti_msg_t msg;
+
+    msg.id = dti_msg_id(DTI_PID_C2, node);
+    // 0-1 Brake Current
+    msg.data[0] = (brake_curr >> 8);
+    msg.data[1] = brake_curr;
+    // 2-7 not used
+    msg.data[2] = 0;
+    msg.data[3] = 0;
+    msg.data[4] = 0;
+    msg.data[5] = 0;
+    msg.data[6] = 0;
+    msg.data[7] = 0;
+
+    return msg;
+}
+
+// SET EPRM
+dti_msg_t Compose_C3(dti_node_t node, int32_t erpm)
+{
+    dti_msg_t msg;
+
+    msg.id = dti_msg_id(DTI_PID_C3, node);
+    // Target EPRM
     msg.data[0] = (erpm >> 24);
     msg.data[1] = (erpm >> 16);
     msg.data[2] = (erpm >> 8);
     msg.data[3] = eprm;
+    // 4-7 not used
+    msg.data[4] = 0;
+    msg.data[5] = 0;
+    msg.data[6] = 0;
+    msg.data[7] = 0;
 
+    return msg;
+}
+
+// Set position
+dti_msg_t Compose_C4(dti_node_t node, int32_t target_pos)
+{
+    dti_msg_t msg;
+
+    msg.id = dti_msg_id(DTI_PID_C4, node);
+    // 0-1 Target Position
+    msg.data[0] = (target_pos >> 8);
+    msg.data[1] = target_pos;
+    // 2-7 not used
+    msg.data[2] = 0;
+    msg.data[3] = 0;
+    msg.data[4] = 0;
+    msg.data[5] = 0;
+    msg.data[6] = 0;
+    msg.data[7] = 0;
+
+    return msg;
+}
+
+// Set relative ac curr
+dti_msg_t Compose_C5(dti_node_t node, int32_t rel_ac_curr)
+{
+    dti_msg_t msg;
+
+    msg.id = dti_msg_id(DTI_PID_C5, node);
+    // 0-1 Target relative AC Current
+    msg.data[0] = (rel_ac_curr >> 8);
+    msg.data[1] = rel_ac_curr;
+    // 2-7 not used
+    msg.data[2] = 0;
+    msg.data[3] = 0;
+    msg.data[4] = 0;
+    msg.data[5] = 0;
+    msg.data[6] = 0;
+    msg.data[7] = 0;
+
+    return msg;
+}
+
+// Set relative brake curr
+dti_msg_t Compose_C6(dti_node_t node, int32_t rel_brake_curr)
+{
+    dti_msg_t msg;
+
+    msg.id = dti_msg_id(DTI_PID_C6, node);
+    // 0-1 Target relative brake AC Current
+    msg.data[0] = (rel_ac_curr >> 8);
+    msg.data[1] = rel_ac_curr;
+    // 2-7 not used
+    msg.data[2] = 0;
+    msg.data[3] = 0;
+    msg.data[4] = 0;
+    msg.data[5] = 0;
+    msg.data[6] = 0;
+    msg.data[7] = 0;
+
+    return msg;
+}
+
+// COMPOSE C7
+
+// Set Max AC Cur
+dti_msg_t Compose_C8(dti_node_t node, int32_t max_ac_curr)
+{
+    dti_msg_t msg;
+
+    msg.id = dti_msg_id(DTI_PID_C8, node);
+    // 0-1 Maximum AC Current
+    msg.data[0] = (max_ac_curr >> 8);
+    msg.data[1] = max_ac_curr;
+    // 2-7 not used
+    msg.data[2] = 0;
+    msg.data[3] = 0;
+    msg.data[4] = 0;
+    msg.data[5] = 0;
+    msg.data[6] = 0;
+    msg.data[7] = 0;
+
+    return msg;
+}
+
+// Set max brake curr
+dti_msg_t Compose_C9(dti_node_t node, int32_t max_brake_curr)
+{
+    dti_msg_t msg;
+
+    msg.id = dti_msg_id(DTI_PID_C9, node);
+    // 0-1 Target maximum brake AC Current
+    msg.data[0] = (max_brake_curr >> 8);
+    msg.data[1] = max_brake_curr;
+    // 2-7 not used
+    msg.data[2] = 0;
+    msg.data[3] = 0;
+    msg.data[4] = 0;
+    msg.data[5] = 0;
+    msg.data[6] = 0;
+    msg.data[7] = 0;
+
+    return msg;
+}
+
+// Set max DC curr
+dti_msg_t Compose_C10(dti_node_t node, int32_t max_dc_curr)
+{
+    dti_msg_t msg;
+
+    msg.id = dti_msg_id(DTI_PID_C10, node);
+    // 0-1 Target maximum brake AC Current
+    msg.data[0] = (max_dc_curr >> 8);
+    msg.data[1] = max_dc_curr;
+    // 2-7 not used
+    msg.data[2] = 0;
+    msg.data[3] = 0;
+    msg.data[4] = 0;
+    msg.data[5] = 0;
+    msg.data[6] = 0;
+    msg.data[7] = 0;
+
+    return msg;
+}
+
+// Set max DC brake curr
+dti_msg_t Compose_C11(dti_node_t node, int32_t max_dc_brake_curr)
+{
+    dti_msg_t msg;
+
+    msg.id = dti_msg_id(DTI_PID_C11, node);
+    // 0-1 Target maximum brake AC Current
+    msg.data[0] = (max_dc_brake_curr >> 8);
+    msg.data[1] = max_dc_brake_curr;
+    // 2-7 not used
+    msg.data[2] = 0;
+    msg.data[3] = 0;
+    msg.data[4] = 0;
+    msg.data[5] = 0;
+    msg.data[6] = 0;
+    msg.data[7] = 0;
+
+    return msg;
+}
+
+// Drive enable
+dti_msg_t Compose_C12(dti_node_t node, int16_t driveEnableStat)
+{
+    dti_msg_t msg;
+
+    msg.id = dti_msg_id(DTI_PID_C12, node);
+    // 0-1 drive enabled status
+    msg.data[0] = driveEnableStat;
+    
+    // 2-7 not used
+    msg.data[1] = 0;
+    msg.data[2] = 0;
+    msg.data[3] = 0;
     msg.data[4] = 0;
     msg.data[5] = 0;
     msg.data[6] = 0;
